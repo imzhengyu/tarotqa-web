80bc1cc2c79a157980c370136bffbe69a9c34002
imzhengyu <sheol@qq.com>
2026-03-21 09:36:24 +0800

    Fix: add basename for GitHub Pages routing

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -10,7 +10,7 @@ console.log('TarotQA starting...');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
+      <BrowserRouter basename="/tarotqa-web">
         <App />
       </BrowserRouter>
     </ErrorBoundary>
